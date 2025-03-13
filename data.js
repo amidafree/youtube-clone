e353304d54934fad6e0ddff4a876cc3c0b75d1d0
amida <amida.free@gmail.com>
2025-03-13 23:36:50 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -5,7 +5,10 @@ const videos = [{
   title: "Talking Tech and AI with Google CEO Sundar Pichai!",
   account: "Marques Brownlee",
   views: "3.4M",
-  day: "6 month"
+  day: "6 month",
+  videoLink: "https://www.youtube.com/watch?v=n2RNcPRtAiY",
+  channelLink: "https://www.youtube.com/@mkbhd",
+  subscribers: "19M"
 }, {
   thumbnail: "thumbnails/thumbnail-2.webp",
   time: "8:22",
@@ -13,15 +16,21 @@ const videos = [{
   title: "Try Not To Laugh Challenge #9",
   account: "Markiplier",
   views: "19M",
-  day: "4 years"
+  day: "4 years",
+  videoLink: "https://www.youtube.com/watch?v=mP0RAo9SKZk",
+  channelLink: "https://www.youtube.com/@markiplier",
+  subscribers: "37M"
 }, {
   thumbnail: "thumbnails/thumbnail-3.webp",
   time: "9:13",
   icon: "channel-pictures/channel-3.jpeg",
   title: "Crazy Tik Toks Taken Moments Before DISASTER",
-  account: "SSniperWolf",
+  account: "SSSniperWolf",
   views: "12M",
-  day: "1 year"
+  day: "1 year",
+  videoLink: "https://www.youtube.com/watch?v=FgjPQQeTh1w",
+  channelLink: "https://www.youtube.com/@SSSniperWolf",
+  subscribers: "34M"
 }, {
   thumbnail: "thumbnails/thumbnail-4.webp",
   time: "22:09",
@@ -29,7 +38,10 @@ const videos = [{
   title: "The Simplest Math Problem No One Can Solve - Collatz Conjecture",
   account: "Veritasium",
   views: "18M",
-  day: "4 months"
+  day: "4 months",
+  videoLink: "https://www.youtube.com/watch?v=094y1Z2wpJg",
+  channelLink: "https://www.youtube.com/@veritasium",
+  subscribers: "17M"
 }, {
   thumbnail: "thumbnails/thumbnail-5.webp",
   time: "11:17",
@@ -37,15 +49,21 @@ const videos = [{
   title: "Kadane's Algorithm to Maximum Sum Subarray Problem",
   account: "CS Dojo",
   views: "519K",
-  day: "5 years"
+  day: "5 years",
+  videoLink: "https://www.youtube.com/watch?v=86CQq3pKSUw",
+  channelLink: "https://www.youtube.com/@CSDojo",
+  subscribers: "1.9M"
 }, {
   thumbnail: "thumbnails/thumbnail-6.webp",
   time: "19:59",
   icon: "channel-pictures/channel-6.jpeg",
   title: "Anything You Can Fit In The Circle I’ll Pay For",
-  account: "Anything You Can Fit In The Circle I’ll Pay For",
+  account: "MrBeast",
   views: "141M",
-  day: "1 year"
+  day: "1 year",
+  videoLink: "https://www.youtube.com/watch?v=yXWw0_UfSFg",
+  channelLink: "https://www.youtube.com/@MrBeast",
+  subscribers: "373M"
 }, {
   thumbnail: "thumbnails/thumbnail-7.webp",
   time: "10:13",
@@ -53,7 +71,10 @@ const videos = [{
   title: "Why Planes Don't Fly Over Tibet",
   account: "RealLifeLore",
   views: "6.6M",
-  day: "1 year"
+  day: "1 year",
+  videoLink: "https://www.youtube.com/watch?v=fNVa1qMbF9Y",
+  channelLink: "https://www.youtube.com/@RealLifeLore",
+  subscribers: "7M"
 }, {
   thumbnail: "thumbnails/thumbnail-8.webp",
   time: "7:12",
@@ -61,15 +82,21 @@ const videos = [{
   title: "Inside The World's Biggest Passenger Plane",
   account: "Tech Vision",
   views: "3.7M",
-  day: "10 months"
+  day: "10 months",
+  videoLink: "https://www.youtube.com/watch?v=lFm4EM1juls",
+  channelLink: "https://www.youtube.com/@adin",
+  subscribers: "840K"
 }, {
   thumbnail: "thumbnails/thumbnail-9.webp",
   time: "13:17",
   icon: "channel-pictures/channel-9.jpg",
   title: "The SECRET to Super Human STRENGTH",
-  account: "ThenX",
+  account: "THENX",
   views: "20M",
-  day: "3 years"
+  day: "3 years",
+  videoLink: "https://www.youtube.com/watch?v=ixmxOlcrlUc",
+  channelLink: "https://www.youtube.com/@OFFICIALTHENXSTUDIOS",
+  subscribers: "7M"
 }, {
   thumbnail: "thumbnails/thumbnail-10.webp",
   time: "7:53",
@@ -77,7 +104,10 @@ const videos = [{
   title: "How The World's Largest Cruise Ship Makes 30,000 Meals Every",
   account: "Business Insider",
   views: "42M",
-  day: "1 year"
+  day: "1 year",
+  videoLink: "https://www.youtube.com/watch?v=R2vXbFp5C9o",
+  channelLink: "https://www.youtube.com/@BusinessInsider",
+  subscribers: "9M"
 }, {
   thumbnail: "thumbnails/thumbnail-11.webp",
   time: "4:10",
@@ -85,7 +115,10 @@ const videos = [{
   title: "Dubai's Crazy Underwater Train and Other Things #Only in Dubai",
   account: "Destination Tips",
   views: "3M",
-  day: "1 year"
+  day: "1 year",
+  videoLink: "https://www.youtube.com/watch?v=0nZuYyXET3s",
+  channelLink: "https://www.youtube.com/@Destinationtips",
+  subscribers: "200K"
 }, {
   thumbnail: "thumbnails/thumbnail-12.webp",
   time: "4:51",
@@ -93,7 +126,10 @@ const videos = [{
   title: "What would happen if you didn’t drink water? - Mia Nacamulli",
   account: "TED-Ed",
   views: "12M",
-  day: "5 years"
+  day: "5 years",
+  videoLink: "https://www.youtube.com/watch?v=9iMGFqMmUFs",
+  channelLink: "https://www.youtube.com/@TEDEd",
+  subscribers: "21M"
 }];
 
 export default videos;
--- script.js
+++ script.js
@@ -8,19 +8,40 @@ videos.forEach((video) => {
   renderedHTML += `
     <div class="video-preview">
       <div class="thumbnail-row">
-        <img class="thumbnail" src="${video.thumbnail}">
-        <div class="video-time">${video.time}</div>
+        <a href="${video.videoLink}">
+          <img class="thumbnail" src="${video.thumbnail}">
+          <div class="video-time">${video.time}</div>
+        </a>
       </div>
       <div class="video-info-grid">
         <div class="channel-picture">
-          <img class="profile-picture" src="${video.icon}">
+          <div class="profile-picture-container">
+            <a href="${video.channelLink}">
+            <img class="profile-picture" src="${video.icon}">
+          </a>
+          <div class="channel-tooltip">
+            <img class="channel-tooltip-picture" src="${video.icon}">
+            <div>
+              <div class="channel-tooltip-name">
+                ${video.account}
+              </div>
+              <div class="channel-tooltip-stats">
+                ${video.subscribers} subscribers
+              </div>
+            </div>
+          </div>
+          </div>
         </div>
         <div class="video-info">
           <p class="video-title">
-            ${video.title}
+            <a href="${video.videoLink}">
+              ${video.title}
+            </a>
           </p>
           <p class="video-author">
-            ${video.account}
+            <a href="${video.channelLink}">
+              ${video.account}
+            </a>
           </p>
           <p class="video-stats">
             ${video.views} views &#183; ${video.day} ago
